44eea1b8109608e988463cbe9b62f50d07cf7e48
stevenliao <steven.liaouey@gmail.com>
2022-12-09 12:55:18 -0500

    feat: add day09 solution

---
+++ day09/day09.test.ts
@@ -0,0 +1,33 @@
+import { expect, test } from 'vitest'
+import { part1, part2 } from './day09'
+import fs from 'fs'
+import path from 'path'
+
+const inputString = fs.readFileSync(path.resolve(__dirname, './input.txt'), {
+	encoding: 'utf-8',
+})
+
+const exampleString = fs.readFileSync(path.resolve(__dirname, './example.txt'), {
+	encoding: 'utf-8',
+})
+
+test('part1 examples', () => {
+	expect(part1(exampleString)).toBe(13)
+})
+
+test('part1', () => {
+	expect(part1(inputString)).toBe(6037)
+})
+
+const example2String = fs.readFileSync(path.resolve(__dirname, './example2.txt'), {
+	encoding: 'utf-8',
+})
+
+test('part2 examples', () => {
+	expect(part2(exampleString)).toBe(1)
+	expect(part2(example2String)).toBe(36)
+})
+
+test('part2', () => {
+	expect(part2(inputString)).toBe(2485)
+})
---
+++ day09/day09.ts
@@ -0,0 +1,58 @@
+export const part1 = (input: string) => {
+	const lines = input.split('\n')
+	const seenCoordinateByKnot = getSeenCoordinatesByKnot(lines, 2)
+	return Object.keys(seenCoordinateByKnot[1]).length
+}
+
+export const part2 = (input: string) => {
+	const lines = input.split('\n')
+	const seenCoordinatesByKnot = getSeenCoordinatesByKnot(lines, 10)
+	return Object.keys(seenCoordinatesByKnot[9]).length
+}
+
+const getSeenCoordinatesByKnot = (lines: string[], knotCount: number) => {
+	const coordinateByKnot: Record<number, { x: number; y: number }> = {}
+	const seenCoordinatesByKnot: Record<number, Record<`${number},${number}`, boolean>> = {}
+
+	for (let i = 0; i < knotCount; i++) {
+		coordinateByKnot[i] = { x: 0, y: 0 }
+		seenCoordinatesByKnot[i] = { '0,0': true }
+	}
+
+	lines.forEach((line) => {
+		const [direction, stepStr] = line.split(' ')
+		const step = +stepStr
+
+		for (let i = 0; i < step; i++) {
+			const headCoordinate = coordinateByKnot[0]
+
+			if (direction === 'D') {
+				headCoordinate.y++
+			} else if (direction === 'U') {
+				headCoordinate.y--
+			} else if (direction === 'R') {
+				headCoordinate.x++
+			} else if (direction === 'L') {
+				headCoordinate.x--
+			}
+
+			seenCoordinatesByKnot[0][`${headCoordinate.x},${headCoordinate.y}`] = true
+
+			for (let j = 1; j < knotCount; j++) {
+				const prevCoordinate = coordinateByKnot[j - 1]
+				const coordinate = coordinateByKnot[j]
+				const diffX = prevCoordinate.x - coordinate.x
+				const diffY = prevCoordinate.y - coordinate.y
+
+				if (Math.abs(diffY) >= 2 || Math.abs(diffX) >= 2) {
+					coordinate.x += Math.sign(diffX)
+					coordinate.y += Math.sign(diffY)
+				}
+
+				seenCoordinatesByKnot[j][`${coordinate.x},${coordinate.y}`] = true
+			}
+		}
+	})
+
+	return seenCoordinatesByKnot
+}

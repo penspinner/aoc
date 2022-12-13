c98b4c5f9d21bc3e2dcfd7827b7de997c935526c
stevenliao <steven.liaouey@gmail.com>
2022-12-13 01:07:47 -0500

    feat: add day12 solution

---
+++ 2022/day12/day12.test.ts
@@ -0,0 +1,28 @@
+import { expect, test } from 'vitest'
+import { part1, part2 } from './day12'
+import fs from 'fs'
+import path from 'path'
+
+const inputString = fs.readFileSync(path.resolve(__dirname, './input.txt'), {
+	encoding: 'utf-8',
+})
+
+const exampleString = fs.readFileSync(path.resolve(__dirname, './example.txt'), {
+	encoding: 'utf-8',
+})
+
+test('part1 examples', () => {
+	expect(part1(exampleString)).toBe(31)
+})
+
+test('part1', () => {
+	expect(part1(inputString)).toBe(339)
+})
+
+test('part2 examples', () => {
+	expect(part2(exampleString)).toBe(29)
+})
+
+test('part2', () => {
+	expect(part2(inputString)).toBe(332)
+})
---
+++ 2022/day12/day12.ts
@@ -0,0 +1,109 @@
+const d = [
+	{ dx: 0, dy: 1 },
+	{ dx: 0, dy: -1 },
+	{ dx: 1, dy: 0 },
+	{ dx: -1, dy: 0 },
+]
+
+export const part1 = (input: string) => {
+	return getShortestFrom(input, 'S', 'E', (a: string, b: string) => {
+		return (
+			(a === 'S' && (b === 'a' || b === 'b')) ||
+			(a === 'y' && b === 'E') ||
+			(a === 'z' && b === 'E') ||
+			a === b ||
+			a.charCodeAt(0) + 1 === b.charCodeAt(0) ||
+			(a.charCodeAt(0) > b.charCodeAt(0) && b !== 'E')
+		)
+	})
+}
+
+export const part2 = (input: string) => {
+	return getShortestFrom(input, 'E', 'a', (a: string, b: string) => {
+		return (
+			(a === 'E' && (b === 'z' || b === 'y')) ||
+			(a === 'b' && b === 'S') ||
+			(a === 'a' && b === 'S') ||
+			a === b ||
+			a.charCodeAt(0) - 1 === b.charCodeAt(0) ||
+			(a.charCodeAt(0) < b.charCodeAt(0) && a !== 'E')
+		)
+	})
+}
+
+const getShortestFrom = (
+	input: string,
+	start: string,
+	end: string,
+	canMoveTo: (a: string, b: string) => boolean,
+) => {
+	const lines = input.split('\n')
+	const [startNode, nodesByPosition] = getStartPosition(lines, start, end)
+	const queue = [startNode]
+	let endNode = undefined
+
+	while (queue.length !== 0) {
+		const node = queue.shift()
+
+		if (!node) {
+			break
+		}
+
+		if (node.visited) {
+			continue
+		}
+
+		node.visited = true
+
+		if (node.char === end) {
+			endNode = node
+			break
+		}
+
+		d.forEach(({ dx, dy }) => {
+			const nextNode = nodesByPosition[`${node.x + dx},${node.y + dy}`]
+
+			if (nextNode && !nextNode.visited && canMoveTo(node.char, nextNode.char)) {
+				if (nextNode.distance > node.distance + 1) {
+					nextNode.distance = node.distance + 1
+				}
+
+				queue.push(nextNode)
+			}
+		})
+	}
+
+	if (!endNode) {
+		throw new Error('Could not find end.')
+	}
+
+	return nodesByPosition[`${endNode.x},${endNode.y}`].distance
+}
+
+type Node = { char: string; x: number; y: number; distance: number; visited: boolean }
+
+const getStartPosition = (lines: string[], start: string, end: string) => {
+	const nodesByPosition: Record<`${number},${number}`, Node> = {}
+	let startNode: Node | undefined = undefined
+
+	for (let i = 0; i < lines.length; i++) {
+		for (let j = 0; j < lines[i].length; j++) {
+			const char = lines[i][j]
+			const node = { char, x: j, y: i, distance: 0, visited: false }
+
+			if (char === start) {
+				startNode = node
+				nodesByPosition[`${j},${i}`] = node
+			} else {
+				node.distance = Infinity
+				nodesByPosition[`${j},${i}`] = node
+			}
+		}
+	}
+
+	if (!startNode) {
+		throw new Error(`Unable to find start '${start}'.`)
+	}
+
+	return [startNode, nodesByPosition] as const
+}
